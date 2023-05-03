50657d477223a128cf80bf5127a8cf2b93046830
Alan <babyalan0857@gmail.com>
2023-05-03 20:54:40 +0800

    update from home 1120503

--- TicTacToe_selfpractice.c
+++ TicTacToe_selfpractice.c
@@ -13,7 +13,7 @@ void printBoard()
     printf("---|---|---\n");
     printf(" %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
     printf("---|---|---\n");
-    printf(" %c | %c | %c \n", board[2][0], board[0][1], board[0][2]);
+    printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
     printf("\n");
 }
 void resetBoard()
